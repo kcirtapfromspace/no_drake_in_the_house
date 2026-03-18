9e39ea1f13ba7f1b0160367714c8c1e3ebe6a143
Patrick Deutsch <105461352+kcirtapfromspace@users.noreply.github.com>
2026-03-18 16:52:49 -0600

    fix(frontend): bust cached assets on render deploy

--- frontend/render-entrypoint.sh
+++ frontend/render-entrypoint.sh
@@ -3,6 +3,7 @@ set -eu
 
 : "${BACKEND_UPSTREAM_URL:?BACKEND_UPSTREAM_URL must be set}"
 : "${PORT:=10000}"
+: "${ASSET_VERSION:=$(date +%s)}"
 
 DNS_RESOLVER="${DNS_RESOLVER:-$(awk '/^nameserver[[:space:]]+/ { print $2; exit }' /etc/resolv.conf)}"
 
@@ -13,7 +14,19 @@ fi
 
 BACKEND_UPSTREAM_URL="${BACKEND_UPSTREAM_URL%/}"
 
-export BACKEND_UPSTREAM_URL PORT DNS_RESOLVER
+INDEX_HTML="/usr/share/nginx/html/index.html"
+
+if [ -f "$INDEX_HTML" ]; then
+    tmp_index="$(mktemp)"
+    sed \
+        -e "s|/global.css[^\"']*|/global.css?v=${ASSET_VERSION}|g" \
+        -e "s|/build/bundle.css[^\"']*|/build/bundle.css?v=${ASSET_VERSION}|g" \
+        -e "s|/build/bundle.js[^\"']*|/build/bundle.js?v=${ASSET_VERSION}|g" \
+        "$INDEX_HTML" > "$tmp_index"
+    mv "$tmp_index" "$INDEX_HTML"
+fi
+
+export BACKEND_UPSTREAM_URL PORT DNS_RESOLVER ASSET_VERSION
 
 envsubst '${BACKEND_UPSTREAM_URL} ${PORT} ${DNS_RESOLVER}' \
     < /etc/nginx/templates/default.conf.template \
